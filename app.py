e9be38925851a48732dbde7dd339114adee80e3f
김정우 <jengwookim@gimjeong-uui-MacBookAir.local>
2023-12-01 22:48:08 +0900

    why

---
+++ app.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.write("BTS, 손흥민, 봉준호, 서민혁")
---
+++ main.py
@@ -0,0 +1,30 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    
+@app.get("/")
+async def read_root():
+    return "BTS, 손흥민, 봉준호, 서민혁"
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str,  None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    result = {"item_id": item_id, **item.dict()}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"deleted": item_id}
