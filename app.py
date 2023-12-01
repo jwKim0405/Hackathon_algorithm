4530d028acacf0970b051ff74733fe37dbcdaeac
김정우 <jengwookim@gimjeong-uui-MacBookAir.local>
2023-12-01 23:14:18 +0900

    modify design4

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
 import streamlit as st
 
-# 페이지 제목 및 배경색 변경
+# CSS 스타일 추가
 st.markdown("""
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&display=swap');
+
         body {
+            font-family: 'Nanum Gothic', sans-serif;
             background-color: #ffe4c4;
+            padding: 20px;
         }
+
         h1 {
             text-align: center;
             color: #8B4513;
+            font-size: 36px;
+            margin-bottom: 30px;
         }
+
         h2 {
             color: #8B4513;
+            font-size: 24px;
+            margin-bottom: 15px;
         }
+
         p {
             font-size: 18px;
             color: #8B4513;
+            line-height: 1.6;
+            margin-bottom: 15px;
         }
+
         textarea {
             width: 100%;
             height: 100px;
             font-size: 16px;
+            font-family: 'Nanum Gothic', sans-serif;
             background-color: #f5f5dc;
+            margin-bottom: 15px;
         }
+
         .stButton > button {
             background-color: #8B4513;
             color: #fff;
+            font-family: 'Nanum Gothic', sans-serif;
+        }
+
+        .stNumberInput > input {
+            font-family: 'Nanum Gothic', sans-serif;
         }
     </style>
 """, unsafe_allow_html=True)
@@ -35,15 +57,18 @@ st.markdown("<h1>노인을 위한 이야기 플랫폼</h1>", unsafe_allow_html=T
 
 # 개요 및 개인 정보 입력 섹션
 st.markdown("""
+## 개요
 
+> 어느 누군가의 인생도 한 편의 영화가 될 수 있다.
+>
+> 어떤 사람의 인생은 사람들에게 한 편의 영화가 될 수 있다.
+>
+> **“우리 모두의 인생은 한편의 영화로 쓰여지기에 충분하다”**
+>
 > **“모든 인생은 제대로 쓰이기만 한다면 하나의 소설감이다.”**
 """)
 
-st.markdown("""
-어느 누군가의 삶도 한 편의 감동적인 영화가 될 수 있습니다. 우리들 개개인의 삶의 이야기도, 우리들의 소중한 부모님의 삶도 한 편의 영화가 될 수 있다고 생각합니다. 쓰여지지 않았을 뿐이죠. "노인을 위한 나라는 있다"는 노인들에게 따뜻한 마음으로 다가가고, 그들의 소중한 이야기를 듣고자 하는 플랫폼입니다. 각자의 삶이 하나의 소설처럼 아름다움으로 가득 차 있음을 인식하고, 상호 교류를 통해 삶의 다양한 측면을 공유하며 새로운 학습의 기회를 제공합니다.
-""")
-
-# 개인 정보 입력 섹션
+# 간단한 개인 정보 입력 섹션
 st.markdown("<h2>개인 정보 입력</h2>", unsafe_allow_html=True)
 gender = st.radio("성별", ["남성", "여성"])
 age = st.number_input("나이", min_value=1, max_value=120)
@@ -87,6 +112,6 @@ if st.checkbox("당신은 누군가에게 엄마/아빠였나요?"):
     challenging_moments = st.text_area("자녀로 인해 힘들었던 이야기를 들려주세요.", "")
     would_be_parent_again = st.checkbox("다시 태어나도 당신의 자녀의 어머니/아버지가 되고 싶으신가요?")
 
-# 버튼 스타일 변경
+# 제출 버튼
 if st.button("제출", key="submit_button"):
     st.success("제출되었습니다!")
